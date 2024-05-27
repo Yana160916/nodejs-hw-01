39d6cfd78a5678e0938713f859620f63674a0d34
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-05-27 14:06:00 +0300

    update

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,39 +1,14 @@
-import fs from "fs";
-import path from "node:path";
+import { PATH_DB } from "../constants/contacts.js";
+import fs from "node:fs/promises";
 import { createFakeContact } from "../utils/createFakeContact.js";
 
-const dbPath = path.resolve("src/db/db.json");
-
-function readContacts() {
-  try {
-    const data = fs.readFileSync(dbPath, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      return [];
-    } else {
-      console.error("Error reading contacts:", error);
-      return [];
-    }
+const generateContacts = async (number) => {
+  const contacts = JSON.parse(await fs.readFile(PATH_DB, "utf-8"));
+  for (let i = 0; i < number; i++) {
+    const contact = createFakeContact();
+    contacts.push(contact);
   }
-}
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts), "utf8");
+};
 
-function writeContacts(contacts) {
-  try {
-    fs.writeFileSync(dbPath, JSON.stringify(contacts, null, 2), "utf8");
-  } catch (error) {
-    console.error("Error writing contacts:", error);
-  }
-}
-
-function generateContacts(count) {
-  const existingContacts = readContacts();
-  const newContacts = Array.from({ length: count }, createFakeContact);
-  const updatedContacts = [...existingContacts, ...newContacts];
-  writeContacts(updatedContacts);
-}
-
-const count = process.argv[2] ? parseInt(process.argv[2], 10) : 5;
-
-generateContacts(count);
-console.log(`${count} new contacts generated and added to db.json`);
+await generateContacts(5);
